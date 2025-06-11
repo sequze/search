253fbd16490df093d54fadb9683aef901143897a
Амир Абдрафиков <sequze@mail.ru>
2025-06-11 18:43:12 +0300

    updated user table

--- app/models/user.py
+++ app/models/user.py
@@ -1,12 +1,7 @@
-from typing import AsyncGenerator
-
-from fastapi import Depends
-from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Mapped
+from fastapi_users.db import SQLAlchemyBaseUserTable
 from .base import Base
 
 
 class User(Base, SQLAlchemyBaseUserTable[int]):
-    pass
+    username: Mapped[str]
