c2e7f55fb357d446304146fd0d941b50173cb86a
Амир Абдрафиков <sequze@mail.ru>
2025-06-11 23:13:51 +0300

    add auth router

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,4 +1,12 @@
-from .routes import router
+from .search.routes import router as search_router
+from .auth.routes import router as auth_router, user_router as user_router
+from fastapi import APIRouter
+
+router = APIRouter()
+
+router.include_router(auth_router, prefix="/auth")
+router.include_router(search_router, prefix="/search")
+router.include_router(user_router, prefix="/users")
 
 __all__ = [
     "router",
---
+++ app/api/auth/routes.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter
+from core.auth import fastapi_users, auth_backend
+from core.schemas.user_schemas import UserRead, UserCreate, UserUpdate
+
+router = APIRouter(tags=["Auth"])
+user_router = APIRouter(tags=["Users"])
+user_router.include_router(
+    fastapi_users.get_users_router(UserRead, UserUpdate),
+    prefix="/users",
+    tags=["Users"],
+)
+# /login
+# /logout
+router.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/jwt",
+)
+
+# /register
+router.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+)
+
+# /verify
+router.include_router(
+    fastapi_users.get_verify_router(UserRead),
+)
+
+
+router.include_router(
+    fastapi_users.get_reset_password_router(),
+)
