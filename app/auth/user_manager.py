20871291c7306574ea506b92aa7484fe61d11429
Амир Абдрафиков <sequze@mail.ru>
2025-06-11 18:44:14 +0300

    added user manager

---
+++ app/auth/user_manager.py
@@ -0,0 +1,24 @@
+from typing import Optional
+from fastapi import Request
+from fastapi_users import BaseUserManager, IntegerIDMixin
+from models import User
+
+SECRET = "SECRET"
+
+
+class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
+    reset_password_token_secret = SECRET
+    verification_token_secret = SECRET
+
+    # async def on_after_register(self, user: User, request: Optional[Request] = None):
+    #     print(f"User {user.id} has registered.")
+
+    # async def on_after_forgot_password(
+    #     self, user: User, token: str, request: Optional[Request] = None
+    # ):
+    #     print(f"User {user.id} has forgot their password. Reset token: {token}")
+
+    # async def on_after_request_verify(
+    #     self, user: User, token: str, request: Optional[Request] = None
+    # ):
+    #     print(f"Verification requested for user {user.id}. Verification token: {token}")
