b6c0f2a4d9456ff4df93b63f7b4e88e2ce175822
Амир Абдрафиков <sequze@mail.ru>
2025-06-11 17:31:48 +0300

    add search function

--- app/search.py
+++ app/search.py
@@ -1,9 +1,9 @@
 from models import db_helper, Request
-from models.db_helpers import RequestMongoModel, db_helper
+from models.db_helpers import RequestMongoModel
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
-from motor.motor_asyncio import AsyncIOMotorClient
 import re
+
 # import asyncio
 
 # async def search_postgres():
@@ -17,13 +17,38 @@ async def search(
     request: str,
     limit: int,
     postgres_session: AsyncSession,
-    mysql_session: AsyncSession):
+    mysql_session: AsyncSession,
+):
     result = []
     regex = re.compile(f".*{re.escape(request)}.*", re.IGNORECASE)
-    result.append([i.name for i in (await postgres_session.scalars(select(Request).where(Request.name.like(f"%{request}%")).limit(limit))).all()])
-    result.append([i.name for i in (await mysql_session.scalars(select(Request).where(Request.name.like(f"%{request}%")).limit(limit))).all()])
-    result.append([i.name for i in await RequestMongoModel.find({"name": {"$regex": regex}}).limit(limit).to_list()])
+    result.append(
+        [i.name.strip() for i in (
+                await postgres_session.scalars(
+                    select(Request)
+                    .where(Request.name.like(f"%{request}%"))
+                    .limit(limit)
+                )).all()
+        ]
+    )
+    result.append(
+        [i.name.strip() for i in (
+                await mysql_session.scalars(
+                    select(Request)
+                    .where(Request.name.like(f"%{request}%"))
+                    .limit(limit)
+                )).all()
+        ]
+    )
+    result.append(
+        [
+            i.name.strip()
+            for i in await RequestMongoModel.find({"name": {"$regex": regex}})
+            .limit(limit)
+            .to_list()
+        ]
+    )
     return result
+
 
 # def main():
 #     asyncio.run(search_postgres())
