62af3f7b87a9c765d5fcd5ebe5a945d908b538c4
Амир Абдрафиков <sequze@mail.ru>
2025-06-11 18:44:39 +0300

    added auth backend

--- app/auth/helpers.py
+++ app/auth/helpers.py
@@ -1,5 +1,9 @@
-from fastapi_users.authentication import BearerTransport
-from fastapi_users.authentication import JWTStrategy
+from fastapi_users.authentication import (
+    BearerTransport,
+    JWTStrategy,
+    AuthenticationBackend,
+)
+
 from config import settings
 # TODO: изменить на нормальный путь
 bearer_transport = BearerTransport(tokenUrl="auth/jwt/login")
@@ -10,3 +14,10 @@ def get_jwt_strategy() -> JWTStrategy:
         secret=settings.auth_config.private_key_path.read_text(),
         public_key=settings.auth_config.public_key_path.read_text(),
         lifetime_seconds=3600)
+
+
+auth_backend = AuthenticationBackend(
+    name="jwt",
+    transport=bearer_transport,
+    get_strategy=get_jwt_strategy,
+)
