5bf25c4db108961f2af0a1a8e1b5e23abbc111fb
Амир Абдрафиков <sequze@mail.ru>
2025-06-11 17:39:39 +0300

    create user class

---
+++ app/models/user.py
@@ -0,0 +1,12 @@
+from typing import AsyncGenerator
+
+from fastapi import Depends
+from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
+from sqlalchemy.orm import sessionmaker
+from .base import Base
+
+
+class User(Base, SQLAlchemyBaseUserTable(int)):
+    pass
