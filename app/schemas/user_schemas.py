16a1f1f85fd0e01b68bf9b3326366c00aebe3378
Амир Абдрафиков <sequze@mail.ru>
2025-06-11 22:27:12 +0300

    little fix

--- app/schemas/user_schemas.py
+++ app/schemas/user_schemas.py
@@ -1,13 +1,17 @@
 from fastapi_users import schemas
 
 
-class UserRead(schemas.BaseUser[int]):
+class BaseSchema:
+    username: str
+
+
+class UserRead(BaseSchema, schemas.BaseUser[int]):
     pass
 
 
-class UserCreate(schemas.BaseUserCreate):
+class UserCreate(BaseSchema, schemas.BaseUserCreate):
     pass
 
 
-class UserUpdate(schemas.BaseUserUpdate):
+class UserUpdate(BaseSchema, schemas.BaseUserUpdate):
     pass
