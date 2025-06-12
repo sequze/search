d80366793bb80383bda5e5c7b51d213cf4b5d37c
Амир Абдрафиков <sequze@mail.ru>
2025-06-12 16:22:30 +0300

    fixed bug with encode algorithm

--- app/core/auth/dependencies.py
+++ app/core/auth/dependencies.py
@@ -20,4 +20,6 @@ def get_jwt_strategy() -> JWTStrategy:
     return JWTStrategy(
         secret=settings.auth_config.private_key_path.read_text(),
         public_key=settings.auth_config.public_key_path.read_text(),
-        lifetime_seconds=3600)
+        lifetime_seconds=3600,
+        algorithm="RS256"
+        )
