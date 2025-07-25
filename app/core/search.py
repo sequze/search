7c80ad01240a58a0b65df250437a7889c5a7f4ee
Amir Abdrafikov <105018355+sequze@users.noreply.github.com>
2025-07-25 13:39:49 +0300

    fix bad commit

--- app/core/search.py
+++ app/core/search.py
@@ -37,38 +37,3 @@ async def search(
     offset_max = page * size
     has_next = page * size < len(result)
     return SearchRequest(results=result[offset_min:offset_max], has_next=has_next)
-
-
-# def main():
-#     asyncio.run(search_postgres())
-
-# main()
-=======
-    result.append(
-        [i.name.strip() for i in (
-                await postgres_session.scalars(
-                    select(Request)
-                    .where(Request.name.like(f"%{request}%"))
-                    .limit(limit)
-                )).all()
-        ]
-    )
-    result.append(
-        [i.name.strip() for i in (
-                await mysql_session.scalars(
-                    select(Request)
-                    .where(Request.name.like(f"%{request}%"))
-                    .limit(limit)
-                )).all()
-        ]
-    )
-    result.append(
-        [
-            i.name.strip()
-            for i in await RequestMongoModel.find({"name": {"$regex": regex}})
-            .limit(limit)
-            .to_list()
-        ]
-    )
-    return result
-
